2a141737ff949bf89169ed65d13a5f99bb5c6939
ifTNT <happyjohn369@gmail.com>
2020-01-09 21:14:19 +0800

    Adjuest project structure

--- src/com/github/nukcsie110/milanos/crypto/CryptoBuffer.java
+++ src/com/github/nukcsie110/milanos/crypto/CryptoBuffer.java
@@ -3,7 +3,7 @@ package com.github.nukcsie110.milanos.crypto;
 import java.nio.ByteBuffer;
 
 public interface CryptoBuffer {
-    ByteBuffer encryptedBuffer;
-    ByteBuffer decryptedBuffer;
-    byte[] SEK
+    //ByteBuffer encryptedBuffer;
+    //ByteBuffer decryptedBuffer;
+    //byte[] SEK
 }
