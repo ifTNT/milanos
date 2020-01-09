29a7e94d30d20f024b1b8d8cdb938195e468a07a
softweight <w0911482396@gmaial.com>
2020-01-10 04:01:33 +0800

    haha you got it

--- src/com/github/nukcsie110/milanos/entrypoint/main.java
+++ src/com/github/nukcsie110/milanos/entrypoint/main.java
@@ -1,5 +1,7 @@
 package com.github.nukcsie110.milanos.entrypoint;
+
 import com.github.nukcsie110.milanos.common.*;
+
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
@@ -19,7 +21,7 @@ public class main {
         new main();
     }
 
-    private static byte[] generateByteSeed(int size){
+    private static byte[] generateByteSeed(int size) {
         Random random = new Random();
         byte[] rtVal = new byte[size];
         random.nextBytes(rtVal);
@@ -30,10 +32,7 @@ public class main {
         try {
             Socket HS_socket = new Socket(HS_address, HS_port);
             HS_socket.setSoTimeout(5000);
-            PrintWriter wtr = new PrintWriter(HS_socket.getOutputStream());
-            wtr.println("GET / HTTP/1.1");
-            wtr.println("");
-            wtr.flush();
+
 
             ObjectInputStream in = new ObjectInputStream(HS_socket.getInputStream());
             relay_list = (ArrayList<RelayInfo>) in.readObject();
@@ -45,42 +44,60 @@ public class main {
         }
     }
 
-    private static byte[] CH_AND_PA(InetAddress Des_IP, byte[] payload) {
-        byte[] output = new byte[4096];
-        byte[] d_b_address = Des_IP.getAddress();
-        int[] rand_array = new int[3];    //決定要走哪三個relay
-        ArrayList<byte[]> SEK_list = new ArrayList<byte[]>(3);    //存放的3把key
-        //生rand出來
-        int now_size = relay_list.size();
-        for (int a = 0; a < rand_array.length; ++a) {    //生3個random出來
-            rand_array[a] = (int) (Math.random() * now_size);
-        }
-        for (int i = 0; i < 3; i++) {    //生成三把SEK
-            byte[] sek = generateByteSeed(256);
-            SEK_list.add(i, sek);
-        }
-        byte[] first_relay = new byte[48];
+    private static class CHAAAAA {
+        private static int[] rand_array = new int[3];    //決定要走哪三個relay
+        private static byte[] CID = new byte[128];
+        private static byte[] TTL = new byte[8];
 
-        //開始包header
-        byte[] CID = new byte[128];
-        byte[] TTL = new byte[8];
-        for(int i=0;i<3;i++){
+
+        public static int get_three(){
+            return  rand_array[2];
+        }
+        public static byte[] make_header(byte[]  adr,byte[] port,byte[] payload){
             byte[] header = new byte[184];
-            RelayInfo temp = relay_list.get(i);
-            int port = temp.address.getPort();
-            InetAddress t_addr = temp.address.getAddress();
-            byte[] b_address = t_addr.getAddress();    //IPv4 32-bits
-            byte[] b_port = ByteBuffer.allocate(16).putInt(port).array();    //port 16-bytes
-            System.arraycopy(CID,0,header,0,CID.length);
-            System.arraycopy(b_address,0,header,128,b_address.length);
-            System.arraycopy(b_port,0,header,160,b_port.length);
-            System.arraycopy(TTL,0,header,176,TTL.length);
-            //call詠翔function
+            System.arraycopy(CID, 0, header, 0, CID.length);
+            System.arraycopy(adr, 0, header, 128, adr.length);
+            System.arraycopy(port, 0, header, 160, port.length);
+            System.arraycopy(TTL, 0, header, 176, TTL.length);
+            byte[] temp = new byte[184+payload.length];
+            System.arraycopy(header, 0, temp, 0, header.length);
+            System.arraycopy(payload, 0, temp, 185, payload.length);
+            return temp;
+        }
 
 
+        public static byte[] CH_AND_PA(InetAddress Des_IP,int d_port, byte[] payload) {
+            byte[] output = new byte[4096];
+            byte[] d_b_address = Des_IP.getAddress();
+            byte[] d_b_port = ByteBuffer.allocate(16).putInt(d_port).array();
+            output = make_header(d_b_address,d_b_port,output);    //第一層
+            ArrayList<byte[]> SEK_list = new ArrayList<byte[]>(3);    //存放的3把key
+            //生random出來
+            int now_size = relay_list.size();
+            for (int a = 0; a < rand_array.length; ++a) {    //生3個random出來
+                rand_array[a] = (int) (Math.random() * now_size);
+            }
+            for (int i = 0; i < 3; i++) {    //生成三把SEK
+                byte[] sek = generateByteSeed(256);
+                SEK_list.add(i, sek);
+            }
+            //開始包header
+
+            for (int i = 0; i < 2; i++) {
+                RelayInfo temp = relay_list.get(i);
+                int port = temp.address.getPort();
+                InetAddress t_addr = temp.address.getAddress();
+                byte[] b_address = t_addr.getAddress();    //IPv4 32-bits
+                byte[] b_port = ByteBuffer.allocate(16).putInt(port).array();    //port 16-bytes
+                output = make_header(b_address,b_port,output);
+                //call詠翔function
+
+            }
+            return output;
+        }
     }
-        return 02020202 + output;
-}
+
+
 
 
     private static void run_Entry_server(int port) throws IOException {    //啟動server
@@ -156,7 +173,7 @@ public class main {
 
         public void newRemoteData(Selector selector, SelectionKey sk) throws IOException {
             ByteBuffer buf = ByteBuffer.allocate(1024);
-            if(remote.read(buf) == -1)
+            if (remote.read(buf) == -1)
                 throw new IOException("disconnected");
             lastData = System.currentTimeMillis();
             buf.flip();
@@ -164,9 +181,9 @@ public class main {
         }
 
         public void newClientData(Selector selector, SelectionKey sk) throws IOException {
-            if(!connected) {
+            if (!connected) {
                 ByteBuffer inbuf = ByteBuffer.allocate(1024);
-                if(client.read(inbuf)<1)
+                if (client.read(inbuf) < 1)
                     return;
                 inbuf.flip();
 
@@ -202,18 +219,22 @@ public class main {
                     remoteAddr = InetAddress.getByName(host);
                     System.out.println(host + remoteAddr);
                 }
-                CH_AND_PA();
-                remote = SocketChannel.open(new InetSocketAddress(remoteAddr, port));
+                byte[] payload = new byte[1024];    //to do
+                inbuf.get(payload,0,4096);
+                int first = CHAAAAA.get_three();
+                CHAAAAA.CH_AND_PA(remoteAddr,port,payload);
+//                remote = SocketChannel.open(new InetSocketAddress(remoteAddr, port));
+                remote = SocketChannel.open(new InetSocketAddress(relay_list.get(first).address.getAddress(), relay_list.get(first).address.getPort()));
 
-                ByteBuffer out = ByteBuffer.allocate(1024);
-                out.put((byte)0);
+                ByteBuffer out = ByteBuffer.allocate(4096);
+                out.put((byte) 0);
                 out.put((byte) (remote.isConnected() ? 0x5a : 0x5b));
                 out.putShort((short) port);
                 out.put(remoteAddr.getAddress());
                 out.flip();
                 client.write(out);
 
-                if(!remote.isConnected())
+                if (!remote.isConnected())
                     throw new IOException("connect failed");
 
                 remote.configureBlocking(false);
@@ -222,7 +243,7 @@ public class main {
                 connected = true;
             } else {
                 ByteBuffer buf = ByteBuffer.allocate(1024);
-                if(client.read(buf) == -1)
+                if (client.read(buf) == -1)
                     throw new IOException("disconnected");
                 lastData = System.currentTimeMillis();
                 buf.flip();
@@ -231,7 +252,7 @@ public class main {
         }
     }
 
-    private static ArrayList <SocksClient> clients = new ArrayList<SocksClient>();
+    private static ArrayList<SocksClient> clients = new ArrayList<SocksClient>();
 
     // utility function
     public SocksClient addClient(SocketChannel s) {
@@ -255,8 +276,8 @@ public class main {
 
         int lastClients = clients.size();
         // select loop
-        HS_get_relay_list();
-        while(true) {
+        //HS_get_relay_list();
+        while (true) {
 
             select.select(1000);
 
@@ -264,7 +285,7 @@ public class main {
             Iterator iterator = keys.iterator();
             while (iterator.hasNext()) {
                 SelectionKey k = (SelectionKey) iterator.next();
-
+                System.out.println("HAHAHA YOU　ＧＯＴ　ＩＴ");
                 if (!k.isValid())
                     continue;
 
@@ -302,32 +323,19 @@ public class main {
             // client timeout check
             for (int i = 0; i < clients.size(); i++) {
                 SocksClient cl = clients.get(i);
-                if((System.currentTimeMillis() - cl.lastData) > 30000L) {
+                if ((System.currentTimeMillis() - cl.lastData) > 30000L) {
                     cl.client.close();
-                    if(cl.remote != null)
+                    if (cl.remote != null)
                         cl.remote.close();
                     clients.remove(cl);
                 }
             }
-            if(clients.size() != lastClients) {
+            if (clients.size() != lastClients) {
                 System.out.println(clients.size());
                 lastClients = clients.size();
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
